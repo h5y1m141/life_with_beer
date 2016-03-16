653de3d31e9b260e74f4c1b430af193d9a19284b
HiroshiOyamada <h5y1m141@gmail.com>
2016-03-16 13:53:20 +0900

    大手含めていくつかのメーカーのseedデーターを投入できるように修正

--- app/models/social_account.rb
+++ app/models/social_account.rb
@@ -1,4 +1,6 @@
 class SocialAccount < ActiveRecord::Base
   belongs_to :brewery, foreign_key: :brewery_id
-  enum account_type: { facebook: 0, twitter: 1, hatena: 2, google_plus: 3 }
+  enum account_type: { facebook: 0, twitter: 1, hatena: 2, google_plus: 3, blog: 4, instagram: 5 }
+  has_many :brewery_social_account_relays, dependent: :destroy
+  has_many :breweries, through: :brewery_social_account_relays
 end
---
+++ db/fixtures/02_brewery.rb
@@ -0,0 +1,68 @@
+breweries = [
+  {
+    name: 'COEDO',
+    web_site: 'http://www.coedobrewery.com/jp/',
+    social_accounts_attributes: [
+      {
+        account_type: 0,
+        url: 'https://www.facebook.com/coedobrewery',
+        comment: 'COEDOFacebook公式ページ'
+      },
+      {
+        account_type: 1,
+        url: 'https://twitter.com/COEDOBREWERY',
+        comment: 'COEDOのTwitter'
+      },
+      {
+        account_type: 4,
+        url: 'http://www.coedobrewery.com/blog/feed/atom/',
+        comment: 'COEDOのblog'
+      },
+      {
+        account_type: 5,
+        url: 'https://www.instagram.com/coedo_usa/',
+        comment: 'Instagram'
+      }
+    ]
+  },
+  {
+    name: '箕面ビール',
+    web_site: 'http://www.minoh-beer.jp/',
+    social_accounts_attributes: [
+      {
+        account_type: 0,
+        url: 'https://www.facebook.com/MINOHBEER/',
+        comment: 'Facebook公式ページ'
+      },
+      {
+        account_type: 1,
+        url: 'https://twitter.com/minohbeer',
+        comment: 'Twitter'
+      },
+      {
+        account_type: 4,
+        url: 'http://minohbeer.blog62.fc2.com/?xml',
+        comment: 'オフィシャルブログ'
+      }
+    ]
+  },
+  {
+    name: 'キリンビール',
+    web_site: 'http://www.kirin.co.jp/',
+    social_accounts_attributes: [
+      {
+        account_type: 0,
+        url: 'https://www.facebook.com/kirinbeer',
+        comment: 'Facebook公式ページ'
+      },
+      {
+        account_type: 1,
+        url: 'https://twitter.com/kirin_brewery',
+        comment: 'Twitter'
+      }
+    ]
+  }
+]
+breweries.each do |brewery_params|
+  Brewery.create(brewery_params)
+end
