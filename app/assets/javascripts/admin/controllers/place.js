7ab9f9af9c48f434cea267abb12f3c1faa4b5084
HiroshiOyamada <h5y1m141@gmail.com>
2016-04-01 09:06:29 +0900

    緯度経度の情報を変更したらGoogleMapの描画を変更するように修正

--- app/assets/javascripts/admin/controllers/place.js
+++ app/assets/javascripts/admin/controllers/place.js
@@ -4,19 +4,34 @@ angular.module('LifeWithBeerApp')
   .controller('PlaceCtrl', ['$scope', function ($scope) {
     $scope.latitude = null;
     $scope.longitude = null;
-    $scope.init = function(latitude, longitude){
-      $scope.latitude = latitude;
-      $scope.longitude = longitude;
-	    var mapOptions = {
-		    zoom: 15,
-		    center: new google.maps.LatLng($scope.latitude, $scope.longitude),
-		    mapTypeId: google.maps.MapTypeId.ROADMAP
-	    },
-	        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions),
-          latlng = new google.maps.LatLng($scope.latitude, $scope.longitude),
+    var renderGoogleMap = function(latitude, longitude){
+	    var latlng = new google.maps.LatLng(latitude, longitude),
+          mapOptions = {
+		        zoom: 15,
+		        center: latlng,
+		        mapTypeId: google.maps.MapTypeId.ROADMAP
+	        },
+	        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions),          
           marker = new google.maps.Marker({
             position: latlng,
             map: map
           });
     };
+    $scope.init = function(latitude, longitude){
+      $scope.latitude = latitude;
+      $scope.longitude = longitude;
+      renderGoogleMap($scope.latitude, $scope.longitude);
+    };
+    $scope.$watch('latitude',function(newVal){
+      if(newVal){
+        $scope.latitude = newVal;
+      }
+      renderGoogleMap($scope.latitude, $scope.longitude);
+    });
+    $scope.$watch('longitude',function(newVal){
+      if(newVal){
+        $scope.longitude = newVal;
+      }
+      renderGoogleMap($scope.latitude, $scope.longitude);
+    });    
   }]);
