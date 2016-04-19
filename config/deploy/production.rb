823f1a84c19bed68a965cc3bebd18aa9129a9c04
HiroshiOyamada <h5y1m141@gmail.com>
2016-04-19 17:36:41 +0900

    Productionのデフォルトブランチ設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
+set :branch, 'master'
 role :web, %w{160.16.208.241}
 server '160.16.208.241', user: 'ops', roles: %w{web}
 fetch(:default_env).merge!(rails_env: :production)
