9ed4ca508b21372a061211cd7c09567767d831a0
HiroshiOyamada <h5y1m141@gmail.com>
2016-05-02 19:36:22 +0900

    緯度経度付きのまいばすけっとの店舗情報

--- app/workers/crawler/my_basket.rb
+++ app/workers/crawler/my_basket.rb
@@ -15,14 +15,18 @@ class Crawler::MyBasket < Crawler::Base
     shop_names = page.xpath('//tr[@class="vcard"]/td[@class="fn org"]').map{|node| node.text }
     postal_codes = page.xpath('//tr[@class="vcard"]/td[@class="postal-code"]').map{|node| node.text }
     prefectures = page.xpath('//tr[@class="vcard"]/td[@class="adr"]').map { |node| node.xpath('./span[@class="region"]').text.squish.match(/東京都|神奈川県/).to_s }
-    addresses = page.xpath('//tr[@class="vcard"]/td[@class="adr"]').map { |node| node.xpath('./span[@class="locality"]').text + node.xpath('./span[@class="street-address"]').text }
+    addresses = page.xpath('//tr[@class="vcard"]/td[@class="adr"]').map { |node| node.xpath('./span[@class="locality"]').text + node.xpath('./span[@class="street-address"]').text.squish }
     telephones = page.xpath('//tr[@class="vcard"]/td[@class="tel"]').map{|node| node.text }
     shop_names.each_with_index do|shop, index|
-      unless prefectures[index].empty?
+      temp_address = addresses[index].gsub('　', ' ').split(' ')
+      unless temp_address.empty?
+        address = temp_address[0]
+        address2 = temp_address[1] ? temp_address[1] : nil
         places.push({
           name: shop,
           prefecture_id: JpPrefecture::Prefecture.find(name: prefectures[index]).code,
-          address: addresses[index],
+          address: address,
+          address2: address2,
           phone_number: telephones[index],
           place_type: 1
         })
