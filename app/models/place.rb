d41086c07a797098bc922ddec37cc956a39ce57a
HiroshiOyamada <h5y1m141@gmail.com>
2016-05-09 14:16:54 +0900

    お店の場所をElasticSearch経由で検索できる基盤整備

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
+require 'elasticsearch/model'
+
 class Place < ActiveRecord::Base
   include JpPrefecture
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   jp_prefecture :prefecture_id
   enum place_type: { restaurant: 0, shop: 1, place: 2 }
   has_many :favorites, as: :favoriteable
@@ -28,5 +32,53 @@ class Place < ActiveRecord::Base
         code: o.code
       }
     end
+  end
+
+  def self.create_index!(options={})
+    client = __elasticsearch__.client
+    settings index: {
+               analysis: {
+                 tokenizer: {
+                   ngram_tokenizer: {
+                     type: 'nGram',
+                     min_gram: '2',
+                     max_gram: '3',
+                     token_chars: ['letter', 'digit']
+                   }
+                 },                 
+                 analyzer: {
+                   ngram_analyzer: {
+                     tokenizer: "ngram_tokenizer"
+                   },
+                   kuromoji_analyzer: {
+                     tokenizer: 'kuromoji_tokenizer',
+                     filter:    ['kuromoji_baseform'],
+                   }
+                 }
+               }
+             } do
+      mapping _source: { enabled: true },
+      properties: { enabled: true, analyzer: 'kuromoji_analyzer' } do
+        indexes :trendword_id, type: :integer, index: 'not_analyzed'
+        indexes :name, type: :string, index: 'analyzed', analyzer: 'kuromoji_analyzer'
+        indexes :location, type: :geo_point, lat_lon: true
+      end
+    end
+    client.indices.delete index: 'places' rescue nil if options[:force]
+    client.indices.create index: 'places',
+      body: {
+        settings: settings.to_hash,
+        mappings: mappings.to_hash
+      }
+  end
+  def as_indexed_json(options = {})
+    {
+      id: id,
+      name: name,
+      address: address,
+      latitude: latitude.to_f,
+      longitude: longitude.to_f,
+      location: "#{latitude.to_f},#{longitude.to_f}"
+    }
   end
 end
