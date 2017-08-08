35532fe6972e4891419f7398d3fd7cf37bee5403
朝倉 香里 <kaori@asakura-no-MacBook-Air.local>
2017-08-08 15:42:07 +0900

    Rails側に通信し、完了後フォーム用要素を非表示にする

--- blog_engine/app/assets/javascripts/blog_engine/articles.js
+++ blog_engine/app/assets/javascripts/blog_engine/articles.js
@@ -3,7 +3,30 @@ window.onload = function() {
     el: '#article-form',
     data: {
       show: false,
-      loading: false
+      loading: false,
+      message: '送信中です',
+      title: '',
+      body: ''
+    },
+    methods: {
+      create: function () {
+        this.loading = !this.loading;
+        var that = this,
+            hostname = window.location.hostname,
+            protocol = window.location.protocol,
+            port =  window.location.port,
+            baseURL = [protocol,'//', hostname,':', port, '/blog/articles'].join('');
+
+        var params = {
+          url: baseURL,
+          method: 'POST',
+          data: { title: that.title, body: that.body }
+        }
+        $.ajax(params).done(function(response){
+          that.message = '送信完了しました';
+          that.show = false;
+        });
+      }
     }
   })
 }
