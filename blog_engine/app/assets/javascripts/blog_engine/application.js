c9ceb597a5c249e4b566707722c0f7a9a938778b
朝倉 香里 <kaori@asakura-no-MacBook-Air.local>
2017-08-15 13:43:48 +0900

    application.jsにarticleModelを追加

--- blog_engine/app/assets/javascripts/blog_engine/application.js
+++ blog_engine/app/assets/javascripts/blog_engine/application.js
@@ -15,4 +15,5 @@
 //= require blog_engine/tether.min
 //= require blog_engine/bootstrap.min
 //= require blog_engine/vue
+//= require blog_engine/models/articleModel
 //= require blog_engine/articles
